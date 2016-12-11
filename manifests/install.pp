56c86aeef97c69cf7e8100cc961429fc3617385e
Jordi Prats <jprats@systemadmin.es>
2016-12-11 12:02:12 +0100

    clamav

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,9 +1,12 @@
 class havp::install inherits havp {
 
+  include ::clamav
+
   if($havp::manage_package)
   {
     package { $havp::params::package_name:
       ensure => $havp::package_ensure,
+      require => Class['clamav'],
     }
   }
 
